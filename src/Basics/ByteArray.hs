c02d7b82c2d06a4c668273b23254622686afe92d
Andrew Martin <andrew.thaddeus@gmail.com>
2021-02-11 16:00:42 -0500

    Mark all ByteArray functions as inline

--- src/Basics/ByteArray.hs
+++ src/Basics/ByteArray.hs
@@ -43,30 +43,39 @@ type T = ByteArray
 type T# = ByteArray#
 
 lift :: A# -> A
+{-# inline lift #-}
 lift = ByteArray
 
 unlift :: A -> A#
+{-# inline unlift #-}
 unlift (ByteArray i) = i
 
 liftMutable :: M# s -> M s
+{-# inline liftMutable #-}
 liftMutable = MutableByteArray
 
 unliftMutable :: M s -> M# s
+{-# inline unliftMutable #-}
 unliftMutable (MutableByteArray i) = i
 
 unsafeFreeze# :: M# s -> State# s -> (# State# s, A# #)
+{-# inline unsafeFreeze# #-}
 unsafeFreeze# = unsafeFreezeByteArray#
 
 index# :: ArrayArray# -> Int# -> T#
+{-# inline index# #-}
 index# = Exts.indexByteArrayArray#
 
 read# :: MutableArrayArray# s -> Int# -> State# s -> (# State# s, T# #)
+{-# inline read# #-}
 read# = Exts.readByteArrayArray#
 
 write# :: MutableArrayArray# s -> Int# -> T# -> State# s -> State# s
+{-# inline write# #-}
 write# = Exts.writeByteArrayArray#
 
 set# :: MutableArrayArray# s -> Int# -> Int# -> T# -> State# s -> State# s
+{-# inline set# #-}
 set# marr off len x s = case len of
   0# -> s
   _ -> set# marr (off +# 1# ) (len -# 1# ) x (write# marr off x s)
