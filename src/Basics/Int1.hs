78adda35087e644cc759a6060c7a40873eded32f
Eric Demko <edemko@layer3com.com>
2023-02-21 11:43:57 -0500

    implement Int1 as a copy of BitBool

---
+++ src/Basics/Int1.hs
@@ -0,0 +1,208 @@
+{-# language BangPatterns #-}
+{-# language DataKinds #-}
+{-# language LambdaCase #-}
+{-# language MagicHash #-}
+{-# language UnboxedTuples #-}
+
+-- This provides an interface to bytearrays in which every
+-- boolean is represented by a single bit.
+module Basics.Int1
+  ( -- Types
+    T
+  , T#
+  , R
+    -- Lifting
+  , lift
+  , unlift
+    -- Compare
+  , eq#
+  , neq#
+    -- Array
+  , read#
+  , write#
+  , index#
+  , set#
+  , uninitialized#
+  , initialized#
+  , copy#
+  , copyMutable#
+  , shrink#
+    -- Constants
+  , def
+    -- Encoding
+  , shows
+  ) where
+
+import Prelude hiding (shows)
+
+import GHC.Exts ((+#),(-#),(*#),(==#),(<#),isTrue#)
+import GHC.Exts (Int#,State#,MutableByteArray#,ByteArray#)
+import GHC.Exts (RuntimeRep(IntRep))
+import GHC.Exts (andI#,orI#,notI#,iShiftL#,iShiftRL#)
+import GHC.Int (Int(I#))
+
+import qualified Prelude
+import qualified GHC.Exts as Exts
+
+type T = Int
+type T# = Int#
+type R = 'IntRep
+
+def :: T
+{-# inline def #-}
+def = 0
+
+lift :: T# -> T
+{-# inline lift #-}
+lift = I#
+
+unlift :: T -> T#
+{-# inline unlift #-}
+unlift (I# n) = n
+
+eq# :: Int# -> Int# -> Int#
+{-# inline eq# #-}
+eq# = (==#)
+
+neq# :: Int# -> Int# -> Int#
+{-# inline neq# #-}
+neq# = (Exts./=#)
+
+splitIndex_ :: Int# -> (# Int#, Int# #)
+{-# inline splitIndex_ #-}
+splitIndex_ bitIx = (# wordIx, intraWordIx #)
+  where
+  wordIx = bitIx `iShiftRL#` 6#
+  intraWordIx = bitIx `andI#` 0x3F#
+
+index# :: ByteArray# -> Int# -> T#
+{-# inline index# #-}
+index# arr i =
+  let !(# wordIx, intraWordIx #) = splitIndex_ i
+      !bitBundle = Exts.indexInt64Array# arr wordIx
+      !bit = bitBundle `andI#` (1# `iShiftL#` intraWordIx)
+   in bit `iShiftRL#` intraWordIx
+
+read# :: MutableByteArray# s -> Int# -> State# s -> (# State# s, T# #)
+{-# inline read# #-}
+read# arr i st =
+  let !(# wordIx, intraWordIx #) = splitIndex_ i
+      !(# st', bitBundle #) = Exts.readInt64Array# arr wordIx st
+      !bit = bitBundle `andI#` (1# `iShiftL#` intraWordIx)
+   in (# st', bit `iShiftRL#` intraWordIx #)
+
+write# :: MutableByteArray# s -> Int# -> T# -> State# s -> State# s
+{-# inline write# #-}
+write# arr i v st =
+  let !(# wordIx, intraWordIx #) = splitIndex_ i
+      !(# st', bitBundle #) = Exts.readInt64Array# arr wordIx st
+      !mask = notI# (1# `iShiftL#` intraWordIx)
+      !bitBundle' = (bitBundle `andI#` mask) `orI#` (v `iShiftL#` intraWordIx)
+   in Exts.writeInt64Array# arr wordIx bitBundle' st'
+
+set# :: MutableByteArray# s -> Int# -> Int# -> T# -> State# s -> State# s
+{-# inline set# #-}
+set# arr off0 len0 v st0 =
+    let subOff = off0 `andI#` 7#
+      -- set non-byte-aligned, initial bits
+        len = min# len0 (8# -# subOff)
+        st' = bitLoop off0 len st0
+        -- set full bytes
+        off' = off0 +# len
+        len' = len0 -# len
+        st'' = writeBytes off' len' st'
+        -- set trailing bits smaller than a byte
+        off'' = off' +# ((len' `iShiftRL#` 3#) `iShiftL#` 3#)
+        len'' = len' `andI#` 7#
+     in bitLoop off'' len'' st''
+  where
+  -- TODO could split bitLoop into writeBitsUnaligned and writeBitsAligned, which would use masking instead of a loop
+  bitLoop _ 0# st = st
+  bitLoop off len st =
+    let st' = write# arr off v st
+     in bitLoop (off +# 1#) (len -# 1#) st'
+  writeBytes off len st =
+    let !offB = off `iShiftRL#` 3#
+        !lenB = len `iShiftRL#` 3#
+     in Exts.setByteArray# arr offB lenB vB st
+  vB = if isTrue# v then 0xFF# else 0#
+
+shrink# :: MutableByteArray# s -> Int# -> State# s -> (# State# s, MutableByteArray# s #)
+{-# inline shrink# #-}
+shrink# marr sz st =
+  let !(# wordSz, subWordSz #) = splitIndex_ sz
+      !paddedSz = wordSz +# if isTrue# (subWordSz ==# 0#) then 0# else 1#
+      !szBytes = paddedSz *# 8#
+      !st' = Exts.shrinkMutableByteArray# marr szBytes st
+   in (# st', marr #)
+
+uninitialized# :: Int# -> State# s -> (# State# s, MutableByteArray# s #)
+{-# inline uninitialized# #-}
+uninitialized# sz st =
+  let !(# wordSz, subWordSz #) = splitIndex_ sz
+      !paddedSz = wordSz +# if isTrue# (subWordSz ==# 0#) then 0# else 1#
+      !szBytes = paddedSz *# 8#
+   in Exts.newByteArray# szBytes st
+
+initialized# :: Int# -> T# -> State# s -> (# State# s, MutableByteArray# s #)
+{-# inline initialized# #-}
+initialized# sz v0 st =
+  let !(# wordSz, subWordSz #) = splitIndex_ sz
+      !paddedSz = wordSz +# if isTrue# (subWordSz ==# 0#) then 0# else 1#
+      !szBytes = paddedSz *# 8#
+      !(# st', marr #) = Exts.newByteArray# szBytes st
+      !v = if isTrue# v0 then notI# 0# else 0#
+   in (# Exts.setByteArray# marr 0# szBytes v st', marr #)
+
+copy# :: MutableByteArray# s -> Int# -> ByteArray# -> Int# -> Int# -> State# s -> State# s
+{-# inline copy# #-}
+copy# dst 0# src 0# len st =
+-- TODO when soff == doff, we can do like set#
+-- first align with naiveCopy, then copy by bytes, then copy the traling bits with naiveCopy
+-- in fact, this can work even when soff - doff divisible by 8
+  let !lenB = len `iShiftRL#` 3#
+      !st' = Exts.copyByteArray# src 0# dst 0# lenB st
+      !off' = lenB `iShiftL#` 3#
+      !len' = len `andI#` 7#
+   in naiveCopy# dst off' src off' len' st'
+copy# dst doff src soff len st = naiveCopy# dst doff src soff len st
+
+naiveCopy# :: MutableByteArray# s -> Int# -> ByteArray# -> Int# -> Int# -> State# s -> State# s
+-- TODO if I had an index64 :: ByteArray# -> off:Int#  -> len:Int# -> Int#
+-- that reads up to `min len 64` unaligned bits starting at off
+-- then I could write whole words at a time after aligning the doff, just as in set#
+naiveCopy# _ _ _ _ 0# st = st
+naiveCopy# dst doff src soff len st =
+  let !v = index# src soff
+      !st' = write# dst doff v st
+   in naiveCopy# dst (doff +# 1#) src (soff +# 1#) (len -# 1#) st'
+
+copyMutable# :: MutableByteArray# s -> Int# -> MutableByteArray# s -> Int# -> Int# -> State# s -> State# s
+{-# inline copyMutable# #-}
+copyMutable# dst 0# src 0# len st =
+-- TODO when soff == doff, we can do like set#
+-- first align with naiveCopyMutable, then copy by bytes, then copy the traling bits with naiveCopyMutable
+-- in fact, this can work even when soff - doff divisible by 8
+  let !lenB = len `iShiftRL#` 3#
+      !st' = Exts.copyMutableByteArray# src 0# dst 0# lenB st
+      !off' = lenB `iShiftL#` 3#
+      !len' = len `andI#` 7#
+   in naiveCopyMutable# dst off' src off' len' st'
+copyMutable# dst doff src soff len st = naiveCopyMutable# dst doff src soff len st
+
+naiveCopyMutable# :: MutableByteArray# s -> Int# -> MutableByteArray# s -> Int# -> Int# -> State# s -> State# s
+-- TODO if I had an index64 :: ByteArray# -> off:Int#  -> len:Int# -> Int#
+-- that reads up to `min len 64` unaligned bits starting at off
+-- then I could write whole words at a time after aligning the doff, just as in set#
+naiveCopyMutable# _ _ _ _ 0# st = st
+naiveCopyMutable# dst doff src soff len st =
+  let !(# st', v #) = read# src soff st
+      !st'' = write# dst doff v st'
+   in naiveCopyMutable# dst (doff +# 1#) src (soff +# 1#) (len -# 1#) st''
+
+shows :: T -> String -> String
+shows = Prelude.shows
+
+min# :: Int# -> Int# -> Int#
+{-# inline min# #-}
+min# a b = if isTrue# (a <# b) then a else b
---
+++ test/Int1.hs
@@ -0,0 +1,201 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE UnboxedTuples #-}
+
+module Int1
+  ( tests
+  ) where
+
+import Basics.Int1
+import Prelude hiding (read)
+
+import Control.Applicative (liftA2)
+import Control.Monad (forM)
+import Control.Monad.ST (runST)
+import Data.Monoid (All(..))
+import Data.Primitive.ByteArray (ByteArray(..),MutableByteArray(..))
+import Data.Proxy (Proxy(Proxy))
+import Data.Word (Word32)
+import GHC.Exts (Int(I#),MutableByteArray#)
+import GHC.ST (ST(ST))
+import GHC.TypeNats (KnownNat)
+import Test.QuickCheck.Instances.Text ()
+import Test.Tasty (TestTree,defaultMain,testGroup,adjustOption)
+import Test.Tasty.HUnit (testCase,(@=?),assertFailure)
+import Test.Tasty.QuickCheck ((===),(=/=),(==>))
+import Test.Tasty.QuickCheck (Gen,testProperty)
+import Unsafe.Coerce (unsafeCoerce)
+
+import qualified Control.Monad.Trans.State.Strict as State
+import qualified Data.Foldable as F
+import qualified Data.List as L
+import qualified Data.List as List
+import qualified Data.Primitive.ByteArray as Prim
+import qualified GHC.Exts as Exts
+import qualified GHC.TypeNats as GHC
+import qualified Test.Tasty.QuickCheck as QC
+
+main :: IO ()
+main = defaultMain
+  $ adjustOption (\_ -> QC.QuickCheckTests 4000)
+  $ tests
+
+tests :: TestTree
+tests = testGroup "basics"
+  [ testGroup "Int1"
+    [ testProperty "lift/unlift are inverse" $ \b ->
+        lift (unlift b) == b
+    , testCase "initialize with False" $
+        let sz = 150
+            actual = runST $ do
+                marr <- initialized sz (Int1 0)
+                forM [0..sz-1] $ \i -> read marr i
+            expected = replicate sz (Int1 0)
+         in actual @=? expected
+    , testCase "initialize with True" $
+        let sz = 150
+            actual = runST $ do
+                marr <- initialized sz (Int1 1)
+                forM [0..sz-1] $ \i -> read marr i
+            expected = replicate sz (Int1 1)
+         in actual @=? expected
+    , testProperty "read/write single bits" $ \xs ->
+        let actual = runST $ do
+              marr <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write marr i x
+              forM [0..length xs - 1] $ \i -> read marr i
+         in actual == xs
+    , testProperty "index/write single bits" $ \xs ->
+        let arr = Prim.runByteArray $ do
+              marr <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write marr i x
+              pure marr
+            actual = index arr <$> [0..length xs - 1]
+         in actual == xs
+    , testProperty "shrink" $ \Shrink{xs,sz} ->
+        let actual = runST $ do
+              marr <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write marr i x
+              marr' <- shrink marr sz
+              forM [0..sz - 1] $ \i -> read marr' i
+         in actual == take sz xs
+    , testProperty "set#" $ \Set{xs,off,len,v} ->
+        let actual = runST $ do
+              marr <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write marr i x
+              set marr off len v
+              forM [0..length xs - 1] $ \i -> read marr i
+            expected = take off xs ++ replicate len v ++ drop (off + len) xs
+         in actual === expected
+    , testProperty "copy" $ \Copy{xs,doff,ys,soff,len} ->
+        let src = Prim.runByteArray $ do
+              marr <- uninitialized (length ys)
+              forM (zip [0..] ys) $ \(i, y) ->
+                write marr i y
+              pure marr
+            actual = runST $ do
+              dst <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write dst i x
+              copy dst doff src soff len
+              forM [0..length xs - 1] $ \i -> read dst i
+            expected = take doff xs ++ take len (drop soff ys) ++ drop (doff + len) xs
+         in actual === expected
+    , testProperty "copyMutable" $ \Copy{xs,doff,ys,soff,len} ->
+        let actual = runST $ do
+              src <- uninitialized (length ys)
+              forM (zip [0..] ys) $ \(i, y) ->
+                write src i y
+              dst <- uninitialized (length xs)
+              forM (zip [0..] xs) $ \(i, x) ->
+                write dst i x
+              copyMutable dst doff src soff len
+              forM [0..length xs - 1] $ \i -> read dst i
+            expected = take doff xs ++ take len (drop soff ys) ++ drop (doff + len) xs
+         in actual === expected
+    ]
+  ]
+
+--- Lift the functions under test ---
+
+index :: ByteArray -> Int -> Int1
+index (ByteArray arr) (I# i) = Int1 $ lift (index# arr i)
+
+read :: MutableByteArray s -> Int -> ST s Int1
+read (MutableByteArray marr) (I# ix) = ST (\st ->
+  let (# st', v #) = read# marr ix st
+   in (# st', Int1 $ lift v #))
+
+write :: MutableByteArray s -> Int -> Int1 -> ST s ()
+write (MutableByteArray marr) (I# i) v = ST (\st ->
+  (# write# marr i (unlift $ unInt1 v) st, () #))
+
+uninitialized :: Int -> ST s (MutableByteArray s)
+uninitialized (I# n) = ST (\st ->
+  let (# st', marr #) = uninitialized# n st
+   in (# st', MutableByteArray marr #))
+
+initialized :: Int -> Int1 -> ST s (MutableByteArray s)
+initialized (I# n) v = ST (\st ->
+  let (# st', marr #) = initialized# n (unlift $ unInt1 v) st
+   in (# st', MutableByteArray marr #))
+
+shrink :: MutableByteArray s -> Int -> ST s (MutableByteArray s)
+shrink (MutableByteArray marr) (I# i) = ST (\st ->
+  let (# st', marr' #) = shrink# marr i st
+   in (# st', MutableByteArray marr' #))
+
+set :: MutableByteArray s -> Int -> Int -> Int1 -> ST s ()
+set (MutableByteArray marr) (I# off) (I# len) v = ST (\st ->
+   (# set# marr off len (unlift $ unInt1 v) st, () #))
+
+copy :: MutableByteArray s -> Int -> ByteArray -> Int -> Int -> ST s ()
+copy (MutableByteArray dst) (I# doff) (ByteArray src) (I# soff) (I# len) = ST (\st ->
+  (# copy# dst doff src soff len st, () #))
+
+copyMutable :: MutableByteArray s -> Int -> MutableByteArray s -> Int -> Int -> ST s ()
+copyMutable (MutableByteArray dst) (I# doff) (MutableByteArray src) (I# soff) (I# len) = ST (\st ->
+  (# copyMutable# dst doff src soff len st, () #))
+
+
+newtype Int1 = Int1 {unInt1 :: Int}
+  deriving (Eq, Ord)
+instance Show Int1 where
+  show (Int1 n) = show n
+instance QC.Arbitrary Int1 where
+  arbitrary = do
+    b <- QC.arbitrary
+    pure . Int1 $ if b then 1 else 0
+
+data Shrink = Shrink { xs::[Int1], sz::Int} deriving (Show)
+instance QC.Arbitrary Shrink where
+  arbitrary = do
+    x' <- QC.arbitrary
+    xs' <- QC.arbitrary
+    let xs = x':xs'
+    sz <- QC.chooseInt (1, length xs)
+    pure Shrink{xs,sz}
+
+data Set = Set { xs::[Int1], off::Int, len::Int, v::Int1 } deriving (Show)
+instance QC.Arbitrary Set where
+  arbitrary = do
+    xs <- QC.arbitrary
+    off <- QC.chooseInt (0,max 0 (length xs - 1))
+    len <- QC.chooseInt (0,length xs - off)
+    v <- QC.arbitrary
+    pure Set{xs,off,len,v}
+
+data Copy = Copy { xs::[Int1],doff::Int,ys::[Int1],soff::Int,len::Int } deriving (Show)
+instance QC.Arbitrary Copy where
+  arbitrary = do
+    xs <- QC.arbitrary
+    doff <- QC.chooseInt (0, max 0 (length xs - 1))
+    ys <- QC.arbitrary
+    soff <- QC.chooseInt (0, max 0 (length ys - 1))
+    len <- QC.chooseInt (0, min (length xs - doff) (length ys - soff))
+    pure Copy{xs,doff,ys,soff,len}
